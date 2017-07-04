3f835b534d3ef4c22fe8698c86eeba287eef7ba8
RkTeddy <838474413@qq.com>
2017-07-04 21:56:18 +0800

    Debug

---
+++ smart-car/smart-car.cpp
@@ -0,0 +1,43 @@
+// smart-car.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+using namespace std;
+using namespace cv;
+
+#define PI 3.1415926
+#define ROW_MIN 600
+#define ROW_MAX 1000
+#define COL_MIN 0
+#define COL_MAX 960
+#define MIDLINE_LENGTH ROW_MAX - ROW_MIN
+
+int nowWay;		//当前所在跑道颜色： 
+				//  0：黄色
+				//  1：白色
+				//  2：黑色
+vector<int> midLine;    //中线
+int startLine;    //起点或终点线
+
+void showVideo();
+
+int main() {
+	showVideo();
+    return 0;
+}
+
+void showVideo() {
+
+	VideoCapture capture(1);
+	namedWindow("video", 1);
+
+	while (1) {
+		Mat frame;
+		capture >> frame;
+		imshow("video", frame);
+		waitKey(30);
+	}
+
+}
+
---
+++ smart-car/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+// TODO:  在此处引用程序需要的其他头文件
+
+#include <opencv2\opencv.hpp>
+#include <math.h>
+#include <time.h>   
+#include <windows.h> 
+#include "stdafx.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
