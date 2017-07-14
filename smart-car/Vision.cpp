4aec895e77f42e84a6acbfdd524429a522dee11a
RkTeddy <838474413@qq.com>
2017-07-14 18:22:34 +0800

    Add method of getting midline

--- smart-car/Vision.cpp
+++ smart-car/Vision.cpp
@@ -1,9 +1,13 @@
-#include "stdafx.h"
 #include "Vision.h"
+#include "opencv2\opencv.hpp"
+#include <iostream>
 
+using namespace cv;
+using namespace std;
 
-Vision::Vision(Mat&)
+Vision::Vision(Mat& img)
 {
+	src = img;
 }
 
 
@@ -12,18 +16,39 @@ Vision::~Vision()
 }
 
 
-Mat Vision::bgrBinarization(int b, int g, int r, int allowence)
-{
-	int error, row, col;
-	Mat dst;
+void Vision::showVision() {
 
-	for (row = 0; row < src.rows; row++) {
+	namedWindow("Source image", 1);
+	imshow("Source image", src);
+	waitKey(0);
+
+	namedWindow("Binary image", 1);
+	imshow("Binary image", bin);
+	waitKey(0);
+
+	namedWindow("Raw Midline", 1);
+	imshow("Raw Midline", rawMidline);
+	waitKey(0);
+}
+
+
+void Vision::bgrBinarization(int b, int g, int r, int allowence) {
+
+	Mat dst(src.rows, src.cols, CV_8UC1, Scalar(0));
+
+	int rows = src.rows;
+	for (int row = 0; row < rows; row++) {
+
 		uchar *srcPtr = src.ptr<uchar>(row);
 		uchar *dstPtr = dst.ptr<uchar>(row);
-		for (col = 0; col < src.cols; col++) {
-			error = abs(srcPtr[col * 3] - 0 - b)
+
+		int cols = src.cols;
+		for (int col = 0; col < cols; col++) {
+
+			int error = abs(srcPtr[col * 3] - 0 - b)
 				+ abs(srcPtr[col * 3 + 1] - 0 - g)
 				+ abs(srcPtr[col * 3 + 2] - 0 - r);
+
 			if (error <= allowence) {
 				dstPtr[col] = 255;
 			}
@@ -32,6 +57,57 @@ Mat Vision::bgrBinarization(int b, int g, int r, int allowence)
 			}
 		}
 
-		return dst;
+		bin = dst;
 	}
+}
+
+
+void Vision::getRawMidline() {
+
+	int leftPoint, rightPoint;
+	int *midPoint = (int*)malloc(bin.rows * sizeof(int));
+
+	Mat dst(bin.rows, bin.cols, CV_8UC1, Scalar(0));
+
+	// 除去噪点
+	blur(bin, bin, Size(10, 10));
+
+	int rows = bin.rows;
+	for (int row = 0; row < rows; row++) {
+
+		uchar *srcPtr = bin.ptr<uchar>(row);
+		uchar *dstPtr = dst.ptr<uchar>(row);
+
+		// 从左往右不断扫描直到检测到有色点或碰到边缘
+		int col = 0;
+		int cols = bin.cols;
+		while (srcPtr[col] == 0 && col < cols - 1)
+			col++;
+
+		// 如果上述检测没碰到边缘
+		if (col != cols - 1) {
+
+			// 将此点设为左点
+			leftPoint = col;
+
+			// 则从右往左检测右点
+			col = cols - 1;
+			while (srcPtr[col] == 0 && col > leftPoint)
+				col--;
+
+			// 如果左右两点不会重合
+			if (leftPoint != col) {
+				// 得到右点
+				rightPoint = col;
+				// 得到中点
+				midPoint[row] = (leftPoint + rightPoint) / 2;
+				//输出到新图像矩阵
+				dstPtr[midPoint[row]] = 255;
+			} else {
+				continue;
+			}
+		}
+	}
+
+	rawMidline = dst;
 }
