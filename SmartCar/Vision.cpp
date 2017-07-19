a528643bac1570bb257731c940afcb558979c0ee
RkTeddy <838474413@qq.com>
2017-07-19 12:39:17 +0800

    Use LSE to fit midline

--- SmartCar/Vision.cpp
+++ SmartCar/Vision.cpp
@@ -1,6 +1,9 @@
 #include "Vision.h"
 #include "opencv2\opencv.hpp"
+#include "dataProcess.h"
+
 #include <iostream>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
@@ -8,6 +11,8 @@ using namespace std;
 Vision::Vision(Mat& img)
 {
 	src = img;
+	rows = src.rows;
+	cols = src.cols;
 }
 
 
@@ -26,23 +31,25 @@ void Vision::showVision() {
 	imshow("Binary image", bin);
 	waitKey(0);
 
-	namedWindow("Raw Midline", 1);
+	namedWindow("Raw midline", 1);
 	imshow("Raw Midline", rawMidline);
+	waitKey(0);
+
+	namedWindow("Fitted line", 1);
+	imshow("Fitted line", fittedLine);
 	waitKey(0);
 }
 
 
 void Vision::bgrBinarization(int b, int g, int r, int allowence) {
 
-	Mat dst(src.rows, src.cols, CV_8UC1, Scalar(0));
+	Mat dst(rows, cols, CV_8UC1, Scalar(0));
 
-	int rows = src.rows;
 	for (int row = 0; row < rows; row++) {
 
 		uchar *srcPtr = src.ptr<uchar>(row);
 		uchar *dstPtr = dst.ptr<uchar>(row);
 
-		int cols = src.cols;
 		for (int col = 0; col < cols; col++) {
 
 			int error = abs(srcPtr[col * 3] - 0 - b)
@@ -56,9 +63,9 @@ void Vision::bgrBinarization(int b, int g, int r, int allowence) {
 				dstPtr[col] = 0;
 			}
 		}
-
-		bin = dst;
 	}
+
+	bin = dst;
 }
 
 
@@ -66,14 +73,11 @@ void Vision::getRawMidline() {
 
 	int leftPoint, rightPoint;
 
-	Mat dst(bin.rows, bin.cols, CV_8UC1, Scalar(0));
-	vector<Point> A;
-	vector<Point> B;
+	Mat dst(rows, cols, CV_8UC1, Scalar(0));
 
 	// 除去噪点
-	blur(bin, bin, Size(10, 10));
+	//blur(bin, bin, Size(10, 10));
 
-	int rows = bin.rows;
 	for (int row = 0; row < rows; row++) {
 
 		uchar *srcPtr = bin.ptr<uchar>(row);
@@ -81,7 +85,6 @@ void Vision::getRawMidline() {
 
 		// 从左往右不断扫描直到检测到有色点或碰到边缘
 		int col = 0;
-		int cols = bin.cols;
 		while (srcPtr[col] == 0 && col < cols - 1)
 			col++;
 
@@ -114,3 +117,21 @@ void Vision::getRawMidline() {
 	rawMidline = dst;
 }
 
+void Vision::fitLine() {
+
+	Mat X = LSE(A, B);
+
+	Mat dst(rows, cols, CV_8UC1, Scalar(0));
+
+	for (int row = 0; row < rows; row++) {
+		uchar *dstPtr = dst.ptr(row);
+		double col = 0;
+		for (int i = 0; i < 2; i++) {
+			uchar *XPtr = X.ptr(i);
+			col += pow(row, (double)i) * (double)XPtr[i];
+		}
+		dstPtr[(int)floor(col)] = 255;
+	}
+
+	fittedLine = dst;
+}
--- SmartCar/Vision.h
+++ SmartCar/Vision.h
@@ -3,6 +3,7 @@
 #include <opencv2\opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 class Vision
 {
@@ -10,9 +11,15 @@ public:
 	Mat src;
 	Mat bin;
 	Mat rawMidline;
+	Mat fittedLine;
+	int rows;
+	int cols;
+	vector<Point> A;
+	vector<Point> B;
 	Vision(cv::Mat&);
 	~Vision();
 	void showVision();
 	void bgrBinarization(int, int, int, int);
 	void getRawMidline();
+	void fitLine();
 };
